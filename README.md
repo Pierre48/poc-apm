6efb0069a80ecbaac0b4d28be705cefb5ceb411c
Pierre Plagnes <pierre.plagnes@gmail.com>
2021-03-23 23:15:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+
+# Elastic
+````
+docker-compose -f docker-compose-elastic.yml up -d
+docker-compose -f docker-compose-elastic.yml down
+````
+# Splunk
+````
+docker-compose -f docker-compose-splunk.yml up -d
+docker-compose -f docker-compose-splunk.yml down
+````
---
+++ src/AppServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using Prometheus;
+
+namespace AppServer
+{
+    class Program
+    {
+        private static readonly Counter TickTock =
+       Metrics.CreateCounter("sampleapp_ticks_total", "Just keeps on ticking");
+
+        static void Main()
+        {
+            var server = new MetricServer(hostname: "appserver", port: 1234);
+            server.Start();
+
+            while (true)
+            {
+                TickTock.Inc();
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
